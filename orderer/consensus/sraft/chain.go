da0dfd69601fe69c85b6022e2079c829f08ff625
ramendersingh <ramendersingh@gmail.com>
2025-07-05 20:37:41 +0000

    sraft/chain.go changes

--- orderer/consensus/sraft/chain.go
+++ orderer/consensus/sraft/chain.go
@@ -2,19 +2,27 @@ package sraft
 
 import (
 	"github.com/ramendersingh/fabric-sraft/orderer/consensus"
+	"github.com/ramendersingh/fabric-sraft/protos/orderer/sraft"
+	"go.uber.org/zap"
+
 )
 
 type Chain struct {
 	consensus.ChainSupport
+	logger          *zap.Logger
 	config          *Config
 	zoneID          string
 	isRootLeader    bool
-	shardReplicator *ShardReplicator
+	shardReplicator map[string]*ShardReplicator
 }
 
 func (c *Chain) Order(env *cb.Envelope, configSeq uint64) error {
 	// Submit transaction to sRaft log
-	return c.shardReplicator.Replicate(env)
+	entry := &sraft.LogEntry{
+		Data: env.Payload,
+		Config: configSeq,
+	}
+	return c.shards[c.zoneID].Replicate(entry)
 }
 
 func (c *Chain) Start() {
