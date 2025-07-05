a35875d518f48ae2b2cbb09c6496d26743819d56
ramendersingh <ramendersingh@gmail.com>
2025-07-05 20:52:42 +0000

    added sraft/communication.go

---
+++ orderer/consensus/sraft/communication.go
@@ -0,0 +1,27 @@
+package sraft
+
+import (
+	"context"
+	"github.com/hyperledger/fabric/protos/orderer/sraft"
+	"google.golang.org/grpc"
+)
+
+type Replicator struct {
+	conn   *grpc.ClientConn
+	client sraft.SRaftClient
+}
+
+func (r *Replicator) AppendShard(req *sraft.AppendShardRequest) (*sraft.AppendShardResponse, error) {
+	return r.client.AppendShard(context.Background(), req)
+}
+
+func NewReplicator(addr string) (*Replicator, error) {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	return &Replicator{
+		conn:   conn,
+		client: sraft.NewSRaftClient(conn),
+	}, nil
+}
